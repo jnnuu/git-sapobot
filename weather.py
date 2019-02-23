893fe89337187446c6c8233cd5e0f81a849f0377
jnnuu <uusihakimo.j@gmail.com>
2019-02-23 22:55:19 +0200

    Wind speed m/s added.

--- weather.py
+++ weather.py
@@ -2,17 +2,22 @@ import requests
 import json
 from datetime import datetime
 import sys
+from pprint import pprint
+
+
+appId = '351eaa3b51487f75f32ab6c2a54e3621'
 
 global city
-kokoviesti = []		# Results of the request are saved this list
-appId = '' 		# Register on openweathermap and enter your appid here
+kokoviesti = [] #Results of the request are saved this list
 
-def makeRequest(city):	# You need to register in openweathermap.org to use this API
-	r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=' + appId  +  '&units=metric')
+city = "Espoo"
+
+def makeRequest(city):
+	r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=' + appId + '&units=metric')
 	global json_object
 	json_object = r.json()
-	kokoviesti.append('{0}:'.format(city))	# City name is the first row of the weather forecast document
-	printResults(city) 			# Calling the printResults function below
+	kokoviesti.append('{0}:'.format(city))
+	printResults(city)
 
 def printResults(city):
 	print("Weather forecast for {0}:".format(city))
@@ -22,11 +27,15 @@ def printResults(city):
 		klo = int(datetime.utcfromtimestamp(aikaunix).strftime('%H'))
 		lampo = int(json_object['list'][x]['main']['temp'])
 		kuvaus = json_object['list'][x]['weather'][0]['description']
+		tuuli = int(json_object['list'][x]['wind']['speed'])
 		aikahuman = datetime.utcfromtimestamp(aikaunix).strftime('%d.%m hour: %H')
-		rivi = ('{0} : {1} *C : {2}'.format(aikahuman, lampo, kuvaus))
-		if (klo == 00):		# Day changed, formatting....
+		rivi = ('{0} : {1} *C : {2}, wind: {3} m/s'.format(aikahuman, lampo, kuvaus, tuuli))
+		if (klo == 00): #Day changed, formatting....
 			kokoviesti.append('-------------------------')
+			print('-------------------------')
 			kokoviesti.append(rivi)
 		else:
-			print(rivi)	# Printing the results in console
+			print(rivi)
 			kokoviesti.append(rivi)
+
+makeRequest(city)
